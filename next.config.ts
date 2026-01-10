c6149db78aa188d7f14bddac0d3be7a9972d9b83
ashiqrahman12 <ashiq461@gmail.com>
2026-01-11 03:58:17 +0600

    Fix: Remove webpack config to fix Turbopack conflict on Vercel build

--- next.config.ts
+++ next.config.ts
@@ -25,22 +25,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  // Webpack config for WASM support
-  webpack: (config, { isServer }) => {
-    // Enable WebAssembly
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
-
-    // Add rule for wasm files
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'webassembly/async',
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
